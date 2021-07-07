7b6365260a7d9d7663a2222fa07d9e4dce4f0b09
mukilanb28 <mukilanb28@gmail.com>
2021-07-07 14:39:51 +0530

    archive users feature added

---
+++ tests/integration/components/toggle-button-test.js
@@ -0,0 +1,27 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | toggle-button', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<ToggleButton />`);
+
+    //assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    // await render(hbs`
+    //   <ToggleButton>
+    //     template block text
+    //   </ToggleButton>
+    // `);
+
+    //assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(this.element.querySelectorAll('label').length, 2);
+  });
+});
