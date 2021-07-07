6183f187a295c741096d917c7b4973676e8ab642
mukilanb28 <mukilanb28@gmail.com>
2021-07-07 14:32:16 +0530

    hj

--- tests/acceptance/users-test.js
+++ tests/acceptance/users-test.js
@@ -7,26 +7,25 @@ module('Acceptance | users', function(hooks) {
 
   test('visiting /users', async function(assert) {
     await visit('/');
-    assert.notEqual(currentURL(), '/users'); //application route
-    //assert.equal(currentURL(), '/users');
+    assert.equal(currentURL(), '/'); //application route
     
     await click('.button'); //See all users button click
-    //await assert.equal(currentURL(), '/users');
+    await assert.equal(currentURL(), '/users');
 
-    //await click('.user-select'); //Click any 1st user card
+    await click('.user-select'); //Click any 1st user card
     
-    // await click('.red-theme'); //Click Archive button
-    // assert.dom('.red-theme').doesNotExist();
-    // assert.equal(this.element.querySelector('.green-theme').innerHTML, 'UnArchive');
+    await click('.red-theme'); //Click Archive button
+    assert.dom('.red-theme').doesNotExist();
+    assert.equal(this.element.querySelector('.green-theme').innerHTML, 'UnArchive');
 
-    // await visit('/users'); //Back to users route
-    // await click(document.querySelector('.disabled')); //Enable All users
+    await visit('/users'); //Back to users route
+    await click(document.querySelector('.disabled')); //Enable All users
 
-    // await click('.user-select'); //Click 1st user card
+    await click('.user-select'); //Click 1st user card
 
-    // await click('.green-theme'); //Click UnArchive button
-    // assert.dom('.green-theme').doesNotExist();
-    // assert.equal(this.element.querySelector('.red-theme').innerHTML, 'Archive');
+    await click('.green-theme'); //Click UnArchive button
+    assert.dom('.green-theme').doesNotExist();
+    assert.equal(this.element.querySelector('.red-theme').innerHTML, 'Archive');
   
   });
 });
