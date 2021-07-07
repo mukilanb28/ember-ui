1d314bfc81ba9eb4689ac333947ee3323ebcc803
mukilanb28 <mukilanb28@gmail.com>
2021-07-07 14:23:36 +0530

    r2

--- tests/acceptance/users-test.js
+++ tests/acceptance/users-test.js
@@ -7,10 +7,11 @@ module('Acceptance | users', function(hooks) {
 
   test('visiting /users', async function(assert) {
     await visit('/');
-    assert.equal(currentURL(), '/'); //application route
+    assert.equal(currentURL(), '/users'); //application route
+    assert.equal(currentURL(), '/users');
     
     await click('.button'); //See all users button click
-    await assert.equal(currentURL(), '/users');
+    //await assert.equal(currentURL(), '/users');
 
     //await click('.user-select'); //Click any 1st user card
     
