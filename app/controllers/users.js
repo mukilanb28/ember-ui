f871a0edac16319048bea872fc0c323e8db0cbd6
mukilanb28 <mukilanb28@gmail.com>
2021-07-07 12:23:04 +0530

    Archive/Unarchive users functinality added

---
+++ app/controllers/users.js
@@ -0,0 +1,10 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+
+export default class UsersController extends Controller {
+    queryParams = ["archived"];
+    
+    @tracked model="";
+    archived=0;
+
+}
