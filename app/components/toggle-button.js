33898aa26729e294fba9f0d86403b6bf742f007e
mukilanb28 <mukilanb28@gmail.com>
2021-07-07 13:25:01 +0530

    Template formatted

--- app/components/toggle-button.js
+++ app/components/toggle-button.js
@@ -1,9 +1,17 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class ToggleButtonComponent extends Component {
+  @tracked isArchived;
+
   @action
-  toggleState(state) {
-    this.args.toggleState(state);
+  showUsers() {
+    this.args.toggleState(0);
+  }
+
+  @action
+  showAllUsers() {
+    this.args.toggleState(1);
   }
 }
